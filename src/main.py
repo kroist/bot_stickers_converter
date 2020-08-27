f8aceea715eb8fef8172dc0b2f2ce446b5a8e007
Max Zub <kroistsmeh@gmail.com>
2020-08-27 15:05:21 +0300

    support photos and stickers

--- src/main.py
+++ src/main.py
@@ -13,27 +13,25 @@ print(config['api']['token'])
 
 bot = telebot.TeleBot(config['api']['token'])
 
-@bot.message_handler(content_types=['document'])
-def get_document(message):
-    file_info = bot.get_file(message.document.file_id)
-    if message.document.file_size > 2000000:
-        bot.reply_to(message, 'maximum file size is 2MB')
-        return
-    print(message.document.file_size, file_info.file_path)
-    print('https://api.telegram.org/file/bot{0}/{1}'.format(config['api']['token'], file_info.file_path))
-    req = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config['api']['token'], file_info.file_path))
-    print(req.status_code)
+def download_and_send_photo(message, file_path):
+    req = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config['api']['token'], file_path))
     if req.status_code == 200:
-        print('kek')
         try:
             file2 = ToPng.convert_to_png(BytesIO(req.content))
             file2.name = 'image.png'
             bot.send_document(message.chat.id, file2)
         except:
             bot.reply_to(message, 'something went wrong, maybe file is not an image')
-        #print(file.headers)
     else:
         bot.reply_to(message, 'something went wrong, please try again')
+
+@bot.message_handler(content_types=['document'])
+def get_document(message):
+    file_info = bot.get_file(message.document.file_id)
+    if message.document.file_size > 2000000:
+        bot.reply_to(message, 'maximum file size is 2MB')
+        return
+    download_and_send_photo(message, file_info.file_path)
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_help(message):
@@ -41,6 +39,20 @@ def handle_help(message):
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
-    bot.reply_to(message, 'send your image in file format, just like you do in @Stickers')
+    print(len(message.photo))
+    max_photo = message.photo[0]
+    for photo in message.photo:
+        if photo.file_size > max_photo.file_size:
+            max_photo = photo
+    file_info = bot.get_file(max_photo.file_id)
+    download_and_send_photo(message, file_info.file_path)
+
+@bot.message_handler(content_types=['sticker'])
+def handle_sticker(message):
+    if message.sticker.is_animated:
+        bot.reply_to(message, 'I (currently) don\' support animated stickers 😔')
+        return
+    file_info = bot.get_file(message.sticker.file_id)
+    download_and_send_photo(message, file_info.file_path)
 
 bot.polling()
